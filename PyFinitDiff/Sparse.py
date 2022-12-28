bb642e6689ea03f10612dafa8da255e1668eb5c9
MartinPdeS <Martin.Poinsinet.de.Sivry@gmail.com>
2022-12-27 19:16:15 -0500

    update

--- PyFinitDiff/Sparse.py
+++ PyFinitDiff/Sparse.py
@@ -241,8 +241,14 @@ class FiniteDifference2D():
 
     def __post_init__(self):
         self.finit_coefficient = FinitCoefficients(derivative=self.derivative, accuracy=self.accuracy)
-        self.triplet = []
+        self._triplet = None
         self.debug = False
+
+    @property
+    def triplet(self):
+        if not self._triplet:
+            self._construct_triplet_()
+        return self._triplet
 
     @property
     def size(self):
@@ -252,68 +258,60 @@ class FiniteDifference2D():
     def shape(self):
         return [self.size, self.size]
 
-    def _get_diagonal_triplet_(self, coefficients: dict, offset: int, symmetry, sign: int):
-        triplet = []
+    def _get_diagonal_triplet_(self, coefficients: dict, offset: int, symmetry = None):
+        triplet = numpy.array([[0, 0, 0]])
+
+        if symmetry in [-1, 1]:
+            coefficients = self.finit_coefficient.Central()
+
         for idx, value in coefficients.items():
             if symmetry == 1:
-                value = (value if idx == 0 else 2 * value if sign * idx > 0 else 0)
+                value = value if idx == 0 else 2 * value
             if symmetry == -1:
-                value = (value if idx == 0 else 0 if sign * idx < 0 else 0)
-            if symmetry in [0, None]:
-                value = value
+                value = value if idx == 0 else 0
+
+            end_idx = self.size - abs(idx * offset)
+            line_0 = numpy.arange(end_idx)
+            line_2 = numpy.ones(line_0.size) * value
 
             if idx < 0:
-                for i in range(0, self.size + idx * offset):
-                    triplet.append((i, i - idx * offset, value))
-            if idx > 0:
-                for i in range(0, self.size - idx * offset):
-                    triplet.append((i + idx * offset, i, value))
+                sub_triplet = numpy.c_[line_0,
+                                       line_0 - idx * offset,
+                                       line_2]
+
             if idx == 0:
-                for i in range(0, self.size):
-                    triplet.append((i, i, value))
+                sub_triplet = numpy.c_[line_0,
+                                       line_0 - idx * offset,
+                                       line_2]
 
-        return Triplet(triplet)
+            if idx > 0:
+                sub_triplet = numpy.c_[line_0 + idx * offset,
+                                       line_0,
+                                       line_2]
 
-    def _set_boundary_(self, symmetry: int, offset: int, sign: int, coefficients: dict = None):
-        triplet = []
-        match symmetry:
-            case 1:
-                triplet = self._get_diagonal_triplet_(coefficients=coefficients, offset=offset, symmetry=symmetry, sign=sign)
+            triplet = numpy.r_[triplet, sub_triplet]
 
-            case -1:
-                triplet = self._get_diagonal_triplet_(coefficients=coefficients, offset=offset, symmetry=symmetry, sign=sign)
-
-            case 0:
-                triplet = self._get_diagonal_triplet_(coefficients=coefficients, offset=offset, symmetry=symmetry, sign=sign)
-
-            case None:
-                triplet = self._get_diagonal_triplet_(coefficients=coefficients, offset=offset, symmetry=symmetry, sign=sign)
-
-        return triplet
+        return Triplet(triplet[1:, :])
 
     def _get_top_boundary_(self):
-        return self._set_boundary_(symmetry=self.symmetries['top'],
+        return self._get_diagonal_triplet_(symmetry=self.symmetries['top'],
                                    coefficients=self.finit_coefficient.Forward(),
-                                   offset=self.n_y,
-                                   sign=+1)
+                                   offset=self.n_y)
 
     def _get_bottom_boundary_(self):
-        return self._set_boundary_(symmetry=self.symmetries['bottom'],
+        return self._get_diagonal_triplet_(symmetry=self.symmetries['bottom'],
                                   coefficients=self.finit_coefficient.Backward(),
-                                  offset=self.n_y,
-                                  sign=-1)
+                                  offset=self.n_y)
 
     def _get_right_boundary_(self):
-        return self._set_boundary_(symmetry=self.symmetries['right'],
+        return self._get_diagonal_triplet_(symmetry=self.symmetries['right'],
                                    coefficients=self.finit_coefficient.Forward(),
-                                   offset=1,
-                                   sign=+1)
+                                   offset=1)
 
     def _get_left_boundary_(self):
-        return self._set_boundary_(symmetry=self.symmetries['left'],
+        return self._get_diagonal_triplet_(symmetry=self.symmetries['left'],
                                    coefficients=self.finit_coefficient.Backward(),
-                                   offset=1,
-                                   sign=-1)
+                                   offset=1)
 
     def _compute_slices_idx_(self):
         for offset in range(1, self.finit_coefficient.offset_index + 1):
@@ -338,47 +336,11 @@ class FiniteDifference2D():
 
         return right_slice_triplet, left_slice_triplet, top_slice_triplet, bottom_slice_triplet
 
-    def _get_zeros_(self, n, type=float):
-        return [numpy.zeros(self.shape).astype(type) for i in range(n)]
-
-    def _generate_empty_meshes_(self):
-        self.x_meshes = NameSpace(right=self._get_zeros_(1)[0],
-                                  left=self._get_zeros_(1)[0],
-                                  center=self._get_zeros_(1)[0])
-
-        self.y_meshes = NameSpace(top=self._get_zeros_(1)[0],
-                                  bottom=self._get_zeros_(1)[0],
-                                  center=self._get_zeros_(1)[0])
-
-    def _slices_meshes_(self):
-        if self.naive:
-            self.y_meshes.bottom = 0
-            self.y_meshes.top = 0
-
-            self.x_meshes.right = 0
-            self.x_meshes.left = 0
-
-        else:
-            self.y_meshes.bottom[~self.slice_bottom] = 0
-            self.y_meshes.top[~self.slice_top] = 0
-            self.y_meshes.center[self.slice_bottom + self.slice_top] = 0
-
-            self.x_meshes.right[~self.slice_right] = 0
-            self.x_meshes.left[~self.slice_left] = 0
-            self.x_meshes.center[self.slice_right + self.slice_left] = 0
-
-    def _add_meshes_(self):
-        self.M = (self.y_meshes.top + self.y_meshes.bottom + self.y_meshes.center) / (self.dx**self.finit_coefficient.derivative)  # Y derivative
-
-        self.M += (self.x_meshes.left + self.x_meshes.right + self.x_meshes.center) / (self.dy**self.finit_coefficient.derivative)  # X derivative
-
-        self.M = sparse.csr_matrix(self.M)
-
     def _get_y_diagonal_triplet_(self):
-        return self._get_diagonal_triplet_(coefficients=self.finit_coefficient.Central(), offset=self.n_y, symmetry=None, sign=1)
+        return self._get_diagonal_triplet_(coefficients=self.finit_coefficient.Central(), offset=self.n_y)
 
     def _get_x_diagonal_triplet_(self):
-        return self._get_diagonal_triplet_(coefficients=self.finit_coefficient.Central(), offset=1, symmetry=None, sign=1)
+        return self._get_diagonal_triplet_(coefficients=self.finit_coefficient.Central(), offset=1)
 
     def _construct_triplet_(self, Addmesh: numpy.ndarray = None):
         right_slice_triplet, left_slice_triplet, top_slice_triplet, bottom_slice_triplet = self._compute_slices_idx_()
@@ -394,7 +356,6 @@ class FiniteDifference2D():
         triplets.y = triplets.y - top_slice_triplet - bottom_slice_triplet
 
         triplets.top.coincide_i(mask=top_slice_triplet)
-
         triplets.bottom.coincide_i(mask=bottom_slice_triplet)
         triplets.right.coincide_i(mask=right_slice_triplet)
         triplets.left.coincide_i(mask=left_slice_triplet)
@@ -403,22 +364,7 @@ class FiniteDifference2D():
 
         total_triplet.add_triplet(triplets.x, triplets.top, triplets.bottom, triplets.left, triplets.right)
 
-        return total_triplet
-
-    def get_triplet(self):
-        return self._construct_triplet_()
-
-    @property
-    def Dense(self):
-        return self.M
-
-    @property
-    def Sparse(self):
-        return sparse.csr_matrix(self.M)
-
-    def _to_triplet_(self):
-        Coordinate = self.Sparse.tocoo()
-        return numpy.array([Coordinate.col, Coordinate.row, Coordinate.data])
+        self._triplet = total_triplet
 
     def Plot(self, Text=False):
         from pylab import cm
--- tests/test_eigensolving.py
+++ tests/test_eigensolving.py
@@ -4,10 +4,10 @@ import numpy
 import matplotlib.pyplot as plt
 from scipy.sparse import linalg
 
-from unittest.mock import patch
+
 from PyFinitDiff.Sparse import FiniteDifference2D as SparseFD, DiagonalTriplet
 from PyFinitDiff.Dense import FiniteDifference2D as DenseFD
-from PyFinitDiff.Utils import plot_mesh
+
 
 def get_circular_mesh_triplet(n_x: int,
                               n_y: int,
@@ -18,55 +18,14 @@ def get_circular_mesh_triplet(n_x: int,
                               value1: float = 1,
                               plot: bool = False):
 
-    x, y = numpy.mgrid[-100:100:complex(n_x),
+    y, x = numpy.mgrid[-100:100:complex(n_x),
                        -100:100:complex(n_y)]
 
     r = numpy.sqrt((x - x_offset)**2 + (y - y_offset)**2)
     mesh = numpy.ones(x.shape) * value0
     mesh[r < radius] = value1
-    if plot:
-        plt.figure()
-        plt.pcolormesh(x[:, 0], y[0, :], mesh.T)
-        plt.show()
 
     return DiagonalTriplet(mesh)
-
-
-def test_compute_eigenmode_sparse_0(n_x: int,
-                              n_y: int,
-                              radius: float,
-                              x_offset: float = 0,
-                              y_offset: float = 0,
-                              value0: float = 0,
-                              value1: float = 1,
-                              plot: bool = False):
-
-    sparse_instance = SparseFD(n_x=n_x, n_y=n_y, dx=1, dy=1, derivative=2, accuracy=2)
-
-    s = 0
-    sparse_instance.symmetries = {'left': s, 'right': s, 'top': s, 'bottom': s}
-
-    laplacian_triplet = sparse_instance.get_triplet()
-    laplacian_triplet.plot()
-
-    mesh_triplet = get_circular_mesh_triplet(n_x=n_x, n_y=n_y, value0=value0, value1=value1, x_offset=x_offset, y_offset=y_offset, radius=radius)
-    mesh_triplet.values *= 5
-
-    dynamic_triplet = laplacian_triplet + mesh_triplet
-    eigen_values, eigen_vectors = linalg.eigs(dynamic_triplet.to_dense(), k=5, which='LM')
-
-    if plot:
-        fig, axes = plt.subplots(1, eigen_values.size, figsize=(10, 5))
-
-        shape = [n_x, n_y]
-
-        for i, ax in enumerate(axes[:]):
-            Vector = eigen_vectors.T[i].real.reshape(shape).T
-            Vector = numpy.flip(Vector, -1)
-            ax.imshow(Vector.T)
-            ax.set_title(f'eigen_values: {eigen_values[i]}')
-
-        plt.show()
 
 
 def test_compute_eigenmode_dense_0(n_x: int,
@@ -89,7 +48,6 @@ def test_compute_eigenmode_dense_0(n_x: int,
 
     dynamic_triplet = FD_instance.M.todense()
 
-    plot_mesh(dynamic_triplet)
     numpy.fill_diagonal(dynamic_triplet, mesh_triplet.to_dense().diagonal())
 
     eigen_values, eigen_vectors = linalg.eigs(dynamic_triplet, k=5, which='LM')
@@ -108,7 +66,63 @@ def test_compute_eigenmode_dense_0(n_x: int,
         plt.show()
 
 
-test_compute_eigenmode_sparse_0(n_x=7, n_y=7, value0=1, value1=5 * 1.4444, radius=50, x_offset=100)
-test_compute_eigenmode_dense_0(n_x=7, n_y=7, value0=1, value1=5 * 1.4444, radius=50, x_offset=100)
+def test_compute_eigenmode_sparse_0(n_x: int,
+                              n_y: int,
+                              radius: float,
+                              x_offset: float = 0,
+                              y_offset: float = 0,
+                              value0: float = 0,
+                              value1: float = 1,
+                              plot: bool = False,
+                              symmetries: dict = {'left': 0, 'right': 0, 'top': 0, 'bottom': 0}):
+
+    sparse_instance = SparseFD(n_x=n_x, n_y=n_y, dx=1, dy=1, derivative=2, accuracy=2, symmetries=symmetries)
+
+    mesh_triplet = get_circular_mesh_triplet(n_x=n_x, n_y=n_y, value0=value0, value1=value1, x_offset=x_offset, y_offset=y_offset, radius=radius)
+
+    dynamic_triplet = sparse_instance.triplet + mesh_triplet
+
+    eigen_values, eigen_vectors = linalg.eigs(dynamic_triplet.to_dense(), k=5, which='LM', sigma=value1)
+
+    if plot:
+        fig, axes = plt.subplots(1, eigen_values.size, figsize=(10, 5))
+
+        shape = [n_x, n_y]
+
+        for i, ax in enumerate(axes[:]):
+            Vector = eigen_vectors.T[i].real.reshape(shape).T
+            Vector = numpy.flip(Vector, -1)
+            ax.imshow(Vector.T)
+            ax.set_title(f'eigen_values: {eigen_values[i]}')
+
+        plt.show()
+
+
+test_kwargs = {'plot': False, 'n_x': 40, 'n_y': 40, 'value0': 1, 'value1': 5 * 1.4444, 'radius': 70}
+
+test_compute_eigenmode_sparse_0(**test_kwargs, x_offset=100, symmetries={'left': 0, 'right': 1, 'top': 0, 'bottom': 0})
+test_compute_eigenmode_sparse_0(**test_kwargs, x_offset=100, symmetries={'left': 0, 'right': 0, 'top': 0, 'bottom': 0})
+test_compute_eigenmode_sparse_0(**test_kwargs, x_offset=100, symmetries={'left': 0, 'right': -1, 'top': 0, 'bottom': 0})
+
+test_compute_eigenmode_sparse_0(**test_kwargs, x_offset=-100, symmetries={'left': 1, 'right': 0, 'top': 0, 'bottom': 0})
+test_compute_eigenmode_sparse_0(**test_kwargs, x_offset=-100, symmetries={'left': 0, 'right': 0, 'top': 0, 'bottom': 0})
+test_compute_eigenmode_sparse_0(**test_kwargs, x_offset=-100, symmetries={'left': -1, 'right': 0, 'top': 0, 'bottom': 0})
+
+test_compute_eigenmode_sparse_0(**test_kwargs, y_offset=100, symmetries={'left': 0, 'right': 0, 'top': 1, 'bottom': 0})
+test_compute_eigenmode_sparse_0(**test_kwargs, y_offset=100, symmetries={'left': 0, 'right': 0, 'top': 0, 'bottom': 0})
+test_compute_eigenmode_sparse_0(**test_kwargs, y_offset=100, symmetries={'left': 0, 'right': 0, 'top': -1, 'bottom': 0})
+
+test_compute_eigenmode_sparse_0(**test_kwargs, y_offset=-100, symmetries={'left': 0, 'right': 0, 'top': 0, 'bottom': 1})
+test_compute_eigenmode_sparse_0(**test_kwargs, y_offset=-100, symmetries={'left': 0, 'right': 0, 'top': 0, 'bottom': 0})
+test_compute_eigenmode_sparse_0(**test_kwargs, y_offset=-100, symmetries={'left': 0, 'right': 0, 'top': 0, 'bottom': -1})
+# test_compute_eigenmode_dense_0(plot=True, n_x=50, n_y=50, value0=1, value1=5 * 1.4444, radius=50, x_offset=100)
+
+
+
+
+
+
+
+
 
 # -
